47112a588c3c41733a8a391b063edfb62bb6291e
renfei-web <52324624@qq.com>
2023-05-26 19:42:43 +0800

    11

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
     time.sleep(3)
 
-    os.system("allure generate ./temps -o ./reports --clean")
+    os.system("allure generate ./target/allure-results ./allure-report --clean")
 
 
     # 将JSON文件转换成HTML格式的测试报告（生成JSON文件路径：outputs/reports/allure; 生成HTML报告路径：outputs/reports/html）
